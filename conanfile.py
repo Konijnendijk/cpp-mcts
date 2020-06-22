136f568751fb04c1dc268b34ebbd7ab1ae699738
Geert Konijnendijk <geert@konijnendijk.info>
2020-06-22 17:38:18 +0200

    Setup testing in CTest and start driving the build process through conan

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,14 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 
 class CPPMCTSConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "catch2/2.12.2"
+    requires = ["catch2/2.12.2", "qt/5.12.9@bincrafters/stable"]
     generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        self.run("cmake -DBUILD_TESTING=TRUE . {}".format(cmake.command_line))
+        self.run("cmake --build . {}".format(cmake.build_config))
+        cmake.test(args=['--', 'ARGS=-T Test'])
